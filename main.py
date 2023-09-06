7b0fc5c02911c5ede22dbe24c48e1de47c60b330
huanranchen <huanran_chen@outlook.com>
2023-09-06 10:38:05 +0800

    fix a bug in simple aug

--- main.py
+++ main.py
@@ -4,21 +4,11 @@ from torch import nn
 from generators.AutoAug import AutoAugment
 
 in_tensor = torch.rand(6, 3, 30, 30)
-# autoaug = AutoAugment()
-# aug = KA.AugmentationSequential(autoaug)
-# aug.train()
-# aug.requires_grad_(True)
-# torch.sum(aug(in_tensor)).backward()
-# for name, module in aug.named_modules():
-#     print(name, module)
-#     for param_name, _ in module.named_parameters():
-#         print(param_name)
-#     print("-" * 30)
-# KA.ColorJitter
-from generators.ops import shear_y, solarize, posterize
+autoaug = AutoAugment()
+aug = KA.AugmentationSequential(autoaug)
+aug.train()
+aug.requires_grad_(True)
+torch.sum(aug(in_tensor)).backward()
+for name, module in aug.named_parameters():
+    print(name, module.grad)
 
-aug = shear_y(1, 3)
-aug(in_tensor).sum().backward()
-for param_name, param in aug.named_parameters():
-    print(param_name, param, param.grad)
-    print("-" * 30)
